21d112d45ab74e9cb7b4561e85fef3a37a10861e
SuraniDS <suranisooriyaarachchi99429@gmail.com>
2023-10-26 15:47:31 +0530

    Implement view Task Approvals for Task Supervisor

--- src/main/java/com/example/demo/controller/TaskRequestController.java
+++ src/main/java/com/example/demo/controller/TaskRequestController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.responseDto.ResponseNewTaskRequestDto;
-import com.example.demo.entity.TaskRequest;
+import com.example.demo.dto.responseDto.*;
+import com.example.demo.exception.UserException;
 import com.example.demo.service.TaskRequestService;
+import com.example.demo.service.TaskSupervisorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,30 +16,30 @@ import java.util.List;
 public class TaskRequestController {
 
     private final TaskRequestService taskRequestService;
+    private final TaskSupervisorService taskSupervisorService;
 
-    public TaskRequestController(TaskRequestService taskRequestService) {
+    public TaskRequestController(TaskRequestService taskRequestService, TaskSupervisorService taskSupervisorService) {
         this.taskRequestService = taskRequestService;
+        this.taskSupervisorService = taskSupervisorService;
     }
 
     @GetMapping("/get")
     public ResponseEntity<String> getTaskRequests() {
-        // Your logic to retrieve task requests
         return ResponseEntity.ok("Task requests");
     }
 
-
-    @PostMapping("/addnewtask")
-    public ResponseEntity<String> addNewTask(@RequestBody TaskRequest taskRequest) {
-        // Your logic to add new task
-        return ResponseEntity.ok("New task added");
-    }
-
-    // Add more endpoints for updating, deleting, etc.
 
     @GetMapping("/getAllnewtaskrequests")
     public ResponseEntity<List<ResponseNewTaskRequestDto>> getAllnewTaskRequests() {
         List<ResponseNewTaskRequestDto> responseDtos = taskRequestService.getAllNewTaskRequests();
         return ResponseEntity.ok(responseDtos);
+    }
+
+    @GetMapping("/task-approvals")
+    @PreAuthorize("hasAuthority('tasksupervisor:read')")
+    public ResponseEntity<List<ResponseTaskApprovalsDto>> getTaskApprovals(@RequestParam("email") String email) throws UserException {
+        List<ResponseTaskApprovalsDto> taskApprovals = taskSupervisorService.getTaskApprovals(email);
+        return ResponseEntity.ok(taskApprovals);
     }
 
 }
